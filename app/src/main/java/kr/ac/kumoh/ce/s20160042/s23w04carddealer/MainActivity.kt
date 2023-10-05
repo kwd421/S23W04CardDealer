4387757d193ca4a050516f156bdf5e2ef919aa96
kwd421 <tonistark@naver.com>
2023-10-05 17:36:36 +0900

    Start

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.kumoh.ce.s20160042.s23w04carddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.drawable.c_10_of_clubs
+    }
+}
