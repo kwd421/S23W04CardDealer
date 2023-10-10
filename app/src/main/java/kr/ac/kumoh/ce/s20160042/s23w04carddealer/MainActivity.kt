1040bb78db7e38e09783928c1db0eac2daf99ea3
kwd421 <tonistark@naver.com>
2023-10-10 12:54:59 +0900

    land

--- app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
@@ -2,11 +2,18 @@ package kr.ac.kumoh.ce.s20160042.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.ce.s20160042.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        R.drawable.c_10_of_clubs
+        //setContentView(R.layout.activity_main)
+        //R.drawable.c_10_of_clubs
+
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)   // 여기까지 하면 joker 나옴
+
+        main.imgCard1.setImageResource(R.drawable.c_king_of_diamonds)   // 카드 덮어쓰기(뒤집기)
     }
 }
