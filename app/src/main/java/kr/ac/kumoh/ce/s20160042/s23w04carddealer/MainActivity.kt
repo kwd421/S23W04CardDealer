ead68e9215e6104a2d3db3b681f57f05e9d1d813
kwd421 <tonistark@naver.com>
2023-10-12 17:05:45 +0900

    카드 이름 가져와서 출력

--- app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
@@ -12,8 +12,35 @@ class MainActivity : AppCompatActivity() {
         //R.drawable.c_10_of_clubs
 
         main = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(main.root)   // 여기까지 하면 joker 나옴
+        setContentView(main.root)   // 여기까지 하면 뒷면 나옴
 
-        main.imgCard1.setImageResource(R.drawable.c_king_of_diamonds)   // 카드 덮어쓰기(뒤집기)
+        //main.imgCard1.setImageResource(R.drawable.c_king_of_diamonds)   // 카드 덮어쓰기(뒤집기)
+        val res = resources.getIdentifier(
+            //TODO: 하드 코딩 제거할 것
+            getCardName(37), "drawable", packageName)
+        main.imgCard1.setImageResource(res)
+    }
+
+    private fun getCardName(c: Int): String {
+        val shape = when(c / 13) {  // switch문 대신
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return if (number in arrayOf("jack", "queen", "king"))
+            "c_${number}_of_${shape}2"
+        else
+            "c_${number}_of_${shape}"
+
     }
 }
