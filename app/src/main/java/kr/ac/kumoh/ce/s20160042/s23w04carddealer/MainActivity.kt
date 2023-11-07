298a20e6bdbb8e03cfdc4d5b036c5b2aa19e768d
kwd421 <tonistark@naver.com>
2023-11-07 12:24:39 +0900

    포커 족보(거의다돼감)

--- app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20160042/s23w04carddealer/MainActivity.kt
@@ -2,13 +2,10 @@ package kr.ac.kumoh.ce.s20160042.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20160042.s23w04carddealer.databinding.ActivityMainBinding
 import java.util.Arrays
-import java.util.Collections
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
@@ -42,69 +39,108 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun isSpades(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "spades" && b == "spades" && c == "spades" &&
-                d == "spades" && e == "spades")
+    private fun shapeID(a: Array<String>, b:String): Boolean {
+        var count = 0
+        for(i in 0..4)
+            if(a[i] == b)
+                count++
+        return count==4
     }
-    private fun isClubs(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "clubs" && b == "clubs" && c == "clubs" &&
-                d == "clubs" && e == "clubs")
+    private fun isClubs(a: Array<String>): Boolean {
+        return shapeID(a, "clubs")
     }
-    private fun isDiamonds(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "diamonds" && b == "diamonds" && c == "diamonds" &&
-                d == "diamonds" && e == "diamonds")
+    private fun isSpades(a: Array<String>): Boolean {
+        return shapeID(a, "spades")
     }
-    private fun isHearts(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "hearts" && b == "hearts" && c == "hearts" &&
-                d == "hearts" && e == "hearts")
+    private fun isDiamonds(a: Array<String>): Boolean {
+        return shapeID(a, "diamonds")
     }
-    private fun isBlack(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "spades" || a == "clubs" &&
-                b == "spades" || b == "clubs" &&
-                c == "spades" || c == "clubs" &&
-                d == "spades" || d == "clubs" &&
-                e == "spades" || e == "clubs")
+    private fun isHearts(a: Array<String>): Boolean {
+        return shapeID(a, "hearts")
     }
-    private fun isRed(a: String, b: String, c: String, d: String, e: String): Boolean {
-        return (a == "hearts" || a == "diamonds" &&
-                b == "hearts" || b == "diamonds" &&
-                c == "hearts" || c == "diamonds" &&
-                d == "hearts" || d == "diamonds" &&
-                e == "hearts" || e == "diamonds")
+    private fun isBlack(a: Array<String>): Boolean {
+        var count = 0
+        for(i in 0..4)
+            if(a[i] == "spades" || a[i] == "clubs")
+                count++
+        return count==4
     }
-
-    private fun isMountain(a: Int, b: Int, c: Int, d: Int, e: Int): Boolean {
-        return (a == 8 && b == 9 && c == 10 && d == 11 && e == 12)  // 9,10,J,Q,K
+    private fun isRed(a: Array<String>): Boolean {
+        var count = 0
+        for(i in 0..4)
+            if(a[i] == "hearts" || a[i] == "diamonds")
+                count++
+        return count==4
     }
-    private fun isBackStraight(a: Int, b: Int, c: Int, d: Int, e: Int): Boolean {
-        return (a == 0 && b == 1 && c == 2 && d == 3 && e == 4) // A,2,3,4,5
+    private fun isMountain(a: Array<Int>): Boolean {
+        return (a[0] == 8 && a[1] == 9 && a[2] == 10 && a[3] == 11 && a[4] == 12)  // 9,10,J,Q,K
     }
-    private fun isStraight(a: Int, b: Int, c: Int, d: Int, e: Int): Boolean {
-        return if(e-d==1 && d-c==1 && c-b==1 && b-a==1) // 2,3,4,5,6~ 8,9,10,J,Q
+    private fun isBackStraight(a: Array<Int>): Boolean {
+        return (a[0] == 0 && a[1] == 1 && a[2] == 2 && a[3] == 3 && a[4] == 4) // A,2,3,4,5
+    }
+    private fun isStraight(a: Array<Int>): Boolean {
+        return if(a[4]-a[3]==1 && a[3]-a[2]==1 && a[2]-a[1]==1 && a[1]-a[0]==1) // 2,3,4,5,6~ 8,9,10,J,Q
             true
-        else if(a == 0 && b == 1 && c == 2 && d == 3 && e == 12)    // K,A,2,3,4
+        else if(a[0] == 0 && a[1] == 1 && a[2] == 2 && a[3] == 3 && a[4] == 12)    // K,A,2,3,4
             true
-        else if(a == 0 && b == 1 && c == 2 && d == 11 && e == 12)   // Q,K,A,2,3
+        else if(a[0] == 0 && a[1] == 1 && a[2] == 2 && a[3] == 11 && a[4] == 12)   // Q,K,A,2,3
             true
-        else if(a == 0 && b == 1 && c == 10 && d == 11 && e == 12)   // J,Q,K,A,2
+        else if(a[0] == 0 && a[1] == 1 && a[2] == 10 && a[3] == 11 && a[4] == 12)   // J,Q,K,A,2
             true
-        else if(a == 0 && b == 9 && c == 10 && d == 11 && e == 12)   // 10,J,Q,K,A
+        else if(a[0] == 0 && a[1] == 9 && a[2] == 10 && a[3] == 11 && a[4] == 12)   // 10,J,Q,K,A
             true
         else
             false
     }
-//    private fun temp (a: Int, b: Int, c: Int, d: Int, e: Int): String {
-//        val (aS, aN) = cardID(a)
-//        val (bS, bN) = cardID(b)
-//        val (cS, cN) = cardID(c)
-//        val (dS, dN) = cardID(d)
-//        val (eS, eN) = cardID(e)
-//        //val num = intArrayOf(aN, bN, cN, dN, eN)
-//
-//
-//        if(isRed(aS, bS, cS, dS, eS) && isStraight(aN, bN, cN, dN, eN) &&
-//            isBlack(aS, bS, cS, dS, eS) && isStraight(aN, bN, cN, dN, eN))
+    private fun isFourCard(a: Array<Int>): Boolean {
+        //TODO: 11122일떄도 fourcard됨
+        return Counter(a) == "Four Card"
+    }
+    private fun isTriple(a: Array<Int>): Boolean {
+        //TODO: 11223일때도 triple나옴
+        return Counter(a) == "Triple"
+    }
+    private fun isTwoPair(a: Array<Int>): Boolean {
+        //TODO: triple과 차이점
+        return Counter(a) == "Two Pair"
+    }
+    private fun isOnePair(a: Array<Int>): Boolean {
+        return Counter(a) == "One Pair"
+    }
+//    private fun isTop(a: Array<Int>):Boolean {
+//        return pairCounter(a) == "Top"
 //    }
+    private fun Counter(a: Array<Int>): String{
+        var count = IntArray(5){0}
+        var pairCount = 0
+
+        for (i in 0..4)
+        {
+            for(j in 0..4)
+            {
+                if(j == i) continue
+                if(a[i] == a[j])
+                    count[i]++
+            }
+        }
+        for(i in 0..4)
+            if(count[i] == 2)
+                pairCount++ // pairCount
+
+        return if(count.contains(4))
+            return "Four Card"
+        else if(count.contains(3) && count.contains(2)) // FullHouse!!
+            return "Full House"
+        else if(count.contains(3) && !count.contains(2)) // Triple!!
+            return "Triple"
+        else if(pairCount == 4)  // abbcc, aabbc, aabcc
+            return "Two Pair"
+        else if(pairCount == 2)  // One Pair,,,
+            return "One Pair"
+        else
+            return "Top"
+    }
+
     private fun getCardRank(a: Int, b: Int, c: Int, d: Int, e: Int): String {
         val r = arrayOf(a, b, c, d, e)
         Arrays.sort(r)  // 오름차순 정렬
@@ -115,22 +151,40 @@ class MainActivity : AppCompatActivity() {
         val (dS, dN) = cardID(r[3])
         val (eS, eN) = cardID(r[4])
 
+        val shapes = arrayOf(aS, bS, cS, dS, eS)
+        val numbers = arrayOf(aN, bN, cN, dN, eN)
 
         //return "${cardID(r[0])}"
 
-        return if() "Royal Straight Flush"
-        else if() "Back Straight Flush"
-        else if() "Straight Flush"
-        else if() "Four Card"
-        else if() "Full House"
-        else if() "Flush"
-        else if() "Mountain"
-        else if() "Back Straight"
-        else if() "Straight"
-        else if() "Triple"
-        else if() "Two Pair"
-        else if() "One Pair"
-        else "Top"
+        return if(isBlack(shapes)&&isMountain(numbers)||
+            isRed(shapes)&&isMountain(numbers)
+            "Royal Straight Flush"
+        else if(isBlack(shapes)&&isBackStraight(numbers)||
+            isRed(shapes)&&isBackStraight(numbers)
+            "Back Straight Flush"
+        else if(isBlack(shapes)&&isStraight(numbers)||
+            isRed(shapes)&&isStraight(numbers))
+            "Straight Flush"
+        else if(isFourCard(numbers))
+            "Four Card"
+        else if()
+            "Full House"
+        else if(isHearts(shapes)||isDiamonds(shapes)||isClubs(shapes)||isSpades(shapes))
+            "Flush"
+        else if(isMountain(numbers))
+            "Mountain"
+        else if(isBackStraight(numbers))
+            "Back Straight"
+        else if(isStraight(numbers))
+            "Straight"
+        else if(isTriple(numbers))
+            "Triple"
+        else if(isTwoPair(numbers))
+            "Two Pair"
+        else if(isOnePair(numbers))
+            "One Pair"
+        else
+            "Top"
 
         //return "${ranks[0]} ${ranks[1]} ${ranks[2]} ${ranks[3]} ${ranks[4]}"
 
